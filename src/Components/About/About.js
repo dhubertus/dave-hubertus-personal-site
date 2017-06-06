d1c1ae6556333ad6c30483dce19eac3e943f55e4
Dave Hubertus <davehubertus@gmail.com>
2017-06-06 12:57:36 -0600

    readme emptied

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,6 @@ export const About = () => {
       <div id='headshot'></div>
       <h3 id='about-header'>ABOUT</h3>
       <p id='personal'>I am a skillful Software Developer with a persistent craving to learn. From my time as a Developer and Chemist, I have developed a passion for innovation, problem solving, and data analytics. My skill set is currently rooted in JavasScript environments, and I am excited to use the fundamentals I have developed to learn new languages and frameworks. I aspire to join a team that values collaboration, agile processes, and developer growth.</p>
-
     </section>
   )
 }
