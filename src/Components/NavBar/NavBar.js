fdd11cfb3853a7c0187b3c698462e55f17b626e0
Dave Hubertus <davehubertus@gmail.com>
2017-06-08 11:19:25 -0600

    scrolling

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,14 +1,67 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Route, Link, Switch } from 'react-router-dom';
+import Scroll from 'react-scroll'
 
 export const NavBar = () => {
+
+  const scrollAbout = () => {
+    setTimeout(() => {
+      return Scroll.scroller.scrollTo('about-container', {
+        duration: 1000,
+        delay: 150,
+        smooth: true
+      })
+    }, 100)
+  }
+  const scrollResume = () => {
+    setTimeout(() => {
+      return Scroll.scroller.scrollTo('resume-container', {
+        duration: 1000,
+        delay: 150,
+        smooth: true
+      })
+    }, 100)
+  }
+  // const scrollPortfolio = () => {
+  //   return Scroll.scroller.scrollTo('about-container', {
+  //     duration: 1000,
+  //     delay: 150,
+  //     smooth: true
+  //   })
+  // }
+  const scrollContact = () => {
+    setTimeout(() => {
+      return Scroll.scroller.scrollTo('contact-container', {
+        duration: 1000,
+        delay: 150,
+        smooth: true
+      })
+    }, 100)
+
+  }
+
   return (
     <div id='nav-link-holder'>
-      <NavLink className='nav-link' activeClassName='selected' to='/about'>ABOUT</NavLink>
-      <NavLink className='nav-link' activeClassName='selected' to='/resume'>RESUME</NavLink>
-      <NavLink className='nav-link' activeClassName='selected' to='/portfolio'>PORTFOLIO</NavLink>
-      <NavLink className='nav-link' activeClassName='selected' to='/contact'>CONTACT</NavLink>
+      <NavLink
+        className='nav-link'
+        activeClassName='selected'
+        to='/about'
+        onClick={() => scrollAbout()}>ABOUT</NavLink>
+      <NavLink
+        className='nav-link'
+        activeClassName='selected'
+        to='/resume'
+        onClick={() => scrollResume()}>RESUME</NavLink>
+      <NavLink
+        className='nav-link'
+        activeClassName='selected'
+        to='/portfolio'>PORTFOLIO</NavLink>
+      <NavLink
+        className='nav-link'
+        activeClassName='selected'
+        to='/contact'
+        onClick={() => scrollContact()}>CONTACT</NavLink>
     </div>
   )
 }
