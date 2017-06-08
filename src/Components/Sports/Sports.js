f8c11f6cadc20e2acfe358e0eb7972731196cc8d
Dave Hubertus <davehubertus@gmail.com>
2017-06-07 21:16:39 -0600

    sports and story section added

---
+++ src/Components/Sports/Sports.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Sports = () => {
+  return (
+    <section id='sports'>
+      <div id='usa'></div>
+      <div id='giants'></div>
+      <div id='knicks'></div>
+      <div id='yankees'></div>
+    </section>
+  )
+}
