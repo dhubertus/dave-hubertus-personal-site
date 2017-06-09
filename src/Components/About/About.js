318ece90d5a9fdcaeceef49e4d76882d4c8589c3
Dave Hubertus <davehubertus@gmail.com>
2017-06-09 15:32:12 -0600

    responsive

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,11 +9,11 @@ export const About = () => {
       <section id='about-container'>
         <div id='headshot'></div>
         <h3 id='about-header'>ABOUT</h3>
-        <p id='personal'>I am a skillful Software Developer with a persistent craving to learn. From my time as a Developer and Chemist, I have developed a passion for innovation, problem solving, and data analytics. My skill set is currently rooted in JavasScript environments, and I am excited to use the fundamentals I have developed to learn new languages and frameworks. I aspire to join a team that values collaboration, agile processes, and developer growth.</p>
+        <p className='personal'>I am a skillful Software Developer with a persistent craving to learn. From my time as a Developer and Chemist, I have developed a passion for innovation, problem solving, and data analytics. My skill set is currently rooted in JavasScript environments, and I am excited to use the fundamentals I have developed to learn new languages and frameworks. I aspire to join a team that values collaboration, agile processes, and developer growth.</p>
       </section>
       <section id='about-container'>
         <h3 id='about-header'>STORY</h3>
-        <p id='personal'>Turkey meatloaf bacon short loin swine jerky. Capicola ham tongue short loin. Fatback filet mignon meatloaf sirloin swine andouille jerky kielbasa chuck turkey porchetta bresaola. Corned beef jerky spare ribs ball tip ground round, capicola tongue rump swine pastrami drumstick pork. Boudin jerky turducken shoulder kevin, pork chop sirloin drumstick cupim prosciutto turkey. Salami ground round turkey corned beef ham fatback.</p>
+        <p className='personal'>Turkey meatloaf bacon short loin swine jerky. Capicola ham tongue short loin. Fatback filet mignon meatloaf sirloin swine andouille jerky kielbasa chuck turkey porchetta bresaola. Corned beef jerky spare ribs ball tip ground round, capicola tongue rump swine pastrami drumstick pork. Boudin jerky turducken shoulder kevin, pork chop sirloin drumstick cupim prosciutto turkey. Salami ground round turkey corned beef ham fatback.</p>
       </section>
       <Sports />
       <Footer />
