4c6d798c90b17eb478b08b415bdfea4cb953a7ec
Dave Hubertus <davehubertus@gmail.com>
2017-06-15 11:34:50 -0600

    hrefs for anchor tags still need to host movie tracker and ufo sightings

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -10,7 +10,10 @@ export const Portfolio = () => {
           <h4>UFO Sightings</h4>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
           <a className='try-it-anchor'>Try It Yourself</a>
-          <a className='repository-anchor'>UFO Sightings GitHub Repository</a>
+          <a
+            className='repository-anchor'
+            href='https://github.com/dhubertus/ufo-sighting-google-maps'
+            target='_blank'>UFO Sightings GitHub Repository</a>
           <div id='ufo-img'>
             <p>HOVER</p>
           </div>
@@ -19,7 +22,10 @@ export const Portfolio = () => {
           <h4>Movie Tracker</h4>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
           <a className='try-it-anchor'>Try It Yourself</a>
-          <a className='repository-anchor'>Movie Tracker GitHub Repository</a>
+          <a
+            className='repository-anchor'
+            href='https://github.com/dhubertus/MovieTracker'
+            target='_blank'>Movie Tracker GitHub Repository</a>
           <div id='movie-tracker-img'>
             <p>HOVER</p>
           </div>
@@ -27,8 +33,14 @@ export const Portfolio = () => {
         <article className='project-card'>
           <h4>Star Wars API</h4>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
-          <a className='try-it-anchor'>Try It Yourself</a>
-          <a className='repository-anchor'>Star Wars API GitHub Repository</a>
+          <a
+            className='try-it-anchor'
+            href='https://swapi-dave-hubertus.herokuapp.com'
+            target='_blank'>Try It Yourself</a>
+          <a
+            className='repository-anchor'
+            href='https://github.com/dhubertus/StarWarsAPI'
+            target='_blank'>Star Wars API GitHub Repository</a>
           <div id='star-wars-img'>
             <p>HOVER</p>
           </div>
@@ -36,8 +48,14 @@ export const Portfolio = () => {
         <article className='project-card'>
           <h4>CloudKicker</h4>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
-          <a className='try-it-anchor'>Try It Yourself</a>
-          <a className='repository-anchor'>CloudKicker GitHub Repository</a>
+          <a
+            className='try-it-anchor'
+            href='https://dhubertus.github.io/CloudKicker'
+            target='_blank'>Try It Yourself</a>
+          <a
+            className='repository-anchor'
+            href='https://github.com/dhubertus/CloudKicker'
+            target='_blank'>CloudKicker GitHub Repository</a>
           <div id='cloud-kicker-img'>
             <p>HOVER</p>
           </div>
@@ -45,8 +63,13 @@ export const Portfolio = () => {
         <article className='project-card'>
           <h4>Breakout</h4>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
-          <a className='try-it-anchor'>Try It Yourself</a>
-          <a className='repository-anchor'>Breakout GitHub Repository</a>
+          <a
+            className='try-it-anchor' href='https://dhubertus.github.io/GameTime-Breakout'
+            target='_blank'>Try It Yourself</a>
+          <a
+            className='repository-anchor'
+            href='https://github.com/dhubertus/GameTime-Breakout'
+            target='_blank'>Breakout GitHub Repository</a>
           <div id='breakout-img'>
             <p>HOVER</p>
           </div>
