91f81d90d94dbc684f33c5bdf830d7b82118b72d
Dave Hubertus <davehubertus@gmail.com>
2017-06-14 20:48:26 -0600

    anchor tags for links to site/repo

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -8,40 +8,45 @@ export const Portfolio = () => {
         <h3 id='portfolio-title'>PORTFOLIO</h3>
         <article className='project-card'>
           <h4>UFO Sightings</h4>
-          <h5>Description</h5>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
+          <a>Try It Yourself</a>
+          <a>UFO Sightings GitHub Repository</a>
           <div id='ufo-img'>
             <p>HOVER</p>
           </div>
         </article>
         <article className='project-card'>
           <h4>Movie Tracker</h4>
-          <h5>Description</h5>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
+          <a>Try It Yourself</a>
+          <a>Movie Tracker GitHub Repository</a>
           <div id='movie-tracker-img'>
             <p>HOVER</p>
           </div>
         </article>
         <article className='project-card'>
           <h4>Star Wars API</h4>
-          <h5>Description</h5>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
+          <a>Try It Yourself</a>
+          <a>Star Wars API GitHub Repository</a>
           <div id='star-wars-img'>
             <p>HOVER</p>
           </div>
         </article>
         <article className='project-card'>
           <h4>CloudKicker</h4>
-          <h5>Description</h5>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
+          <a>Try It Yourself</a>
+          <a>CloudKicker GitHub Repository</a>
           <div id='cloud-kicker-img'>
             <p>HOVER</p>
           </div>
         </article>
         <article className='project-card'>
           <h4>Breakout</h4>
-          <h5>Description</h5>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
+          <a>Try It Yourself</a>
+          <a>Breakout GitHub Repository</a>
           <div id='breakout-img'>
             <p>HOVER</p>
           </div>
