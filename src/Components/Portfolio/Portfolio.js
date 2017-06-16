617e41312ba6823cf37b5b7cfbb435bbce1e3b21
Dave Hubertus <davehubertus@gmail.com>
2017-06-16 15:18:22 -0600

    ufo read me

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -8,9 +8,10 @@ export const Portfolio = () => {
         <h3 id='portfolio-title'>PORTFOLIO</h3>
         <article className='project-card'>
           <h4>UFO Sightings</h4>
-          <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
+          <p>This project is currently in development although the main funcionality has been completed. When searching for an interesting API that contains lat/long data, I came across plenty of polutant and/or green house gas type API's. Although important, its not the most entertaining data to parse through for weeks. When I came across the UFO Sightings API in an internet thread, I read a few claims and knew I found my data set which proved to be much larger then I could have imagined. I am always thinking of new functionality the user may want when diving into the wild world of UFO sightings. Next steps are to learn/add a form of data visualization (ie. chart.js, d3.js, victory.js) that can provide the user with data trends. Trends, such as, shapes viewed per decade, or number of sightings over time.</p>
+          <p id='pitch'>If you're as skeptical as me or even if you believe, you'll enjoy parsing through these claims and wondering, 'What on Earth could they actually have seen?!?'</p>
           <div className='anchor-holder'>
-            <a className='try-it-anchor'>Try It Yourself</a>
+            <a className='try-it-anchor'><s>Try It Yourself</s></a>
             <a
               className='repository-anchor'
               href='https://github.com/dhubertus/ufo-sighting-google-maps'
@@ -24,7 +25,7 @@ export const Portfolio = () => {
           <h4>Movie Tracker</h4>
           <p>Ac aliquam sit aliquam, nulla quis sociosqu in dolor, arcu habitant, est venenatis at fermentum lacinia donec et, convallis at tortor. Bibendum risus lorem ut metus, leo consectetuer justo cursus egestas, vitae dignissim posuere vel, ornare luctus. Lectus nibh cillum lectus pretium tellus, mollis nibh urna debitis. Arcu sapien in sed dolor, et sit praesent nisl class libero, phasellus amet aliquam sit mi pretium pellentesque. Mattis neque diam viverra iaculis, in lectus erat velit proin suspendisse, beatae enim luctus, sodales a gravida ut pellentesque lorem.</p>
           <div className='anchor-holder'>
-            <a className='try-it-anchor'>Try It Yourself</a>
+            <a className='try-it-anchor'><s>Try It Yourself</s></a>
             <a
               className='repository-anchor'
               href='https://github.com/dhubertus/MovieTracker'
