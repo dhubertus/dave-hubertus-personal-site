2237e305580d01dc9aa9a6bbfa5ac7a29c737886
Dave Hubertus <davehubertus@gmail.com>
2017-06-20 12:53:06 -0600

    edited about and story to remove hints of being unemployed

--- src/Components/Sports/Sports.js
+++ src/Components/Sports/Sports.js
@@ -3,10 +3,10 @@ import React from 'react'
 export const Sports = () => {
   return (
     <section id='sports'>
-      <div id='usa'></div>
       <div id='giants'></div>
       <div id='knicks'></div>
       <div id='yankees'></div>
+      <div id='usa'></div>
     </section>
   )
 }
