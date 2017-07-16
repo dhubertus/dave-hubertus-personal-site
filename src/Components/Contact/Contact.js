e1bb0a8977f739fdfd2613ff8031b13c939fb4c9
Dave Hubertus <davehubertus@gmail.com>
2017-07-16 16:26:29 -0600

    added resume modal

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export const Contact = () => {
+
+export const Contact = ({ toggleModal }) => {
   return (
     <section id='contact-container'>
       <section id='contact-method-container'>
@@ -26,16 +27,20 @@ export const Contact = () => {
                 href='https://www.linkedin.com/in/dave-hubertus-04128327/'
                 target='_blank'>LinkedIn</a>
           </article>
-              <article className='contact-method'>
-                <a
-                  className='contact-icon github'
-                  href='https://github.com/dhubertus'
-                  target='_blank'></a>
-                <a
-                  id='github-url'
-                  href='https://github.com/dhubertus'
-                  target='_blank'>GitHub</a>
-                </article>
+          <article className='contact-method'>
+            <a
+              className='contact-icon github'
+              href='https://github.com/dhubertus'
+              target='_blank'></a>
+            <a
+              id='github-url'
+              href='https://github.com/dhubertus'
+              target='_blank'>GitHub</a>
+          </article>
+          <article
+            id='resume-modal'
+            onClick={ () => toggleModal("resume") }>Download Resume
+          </article>
       </section>
     </section>
   )
